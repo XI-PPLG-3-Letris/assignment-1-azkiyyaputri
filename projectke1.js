ce8ef4550e3e27d7d3e833a584ddefd7b0f7a10d
azkiyyaputri <putrijaelaniazkiya7@gmail.com>
2025-10-06 09:38:38 +0700

    Add files via upload

---
+++ projectke1.js
@@ -0,0 +1,32 @@
+let a = 12;
+let b = 3;
+
+const penjumlahan = (a,b) => {
+    console.log(`= ${a+b}`)
+}
+penjumlahan(12,3)
+
+const pengurangan = (a,b) => {
+    console.log(`= ${a-b}`)
+}
+pengurangan(12,3)
+
+const perkalian = (a,b) => {
+    console.log(`= ${a*b}`)
+}
+perkalian(12,3)
+
+const pembagian = (a,b) => {
+    console.log(`= ${a/b}`)
+}
+pembagian(12,3)
+
+const modulus = (a,b) => {
+    console.log(`= ${a%b}`)
+}
+modulus(12,3)
+
+const perpangkatan = (a,b) => {
+    console.log(`= ${a**b}`)
+}
+perpangkatan(12,3)
